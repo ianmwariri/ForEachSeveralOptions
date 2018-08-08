d12d768e3e986c3b557311ee4a5861382fb24371
smokeyice <mwangi.ian3@gmail.com>
2018-08-08 15:33:48 +0300

    Add Several ForEach Options

---
+++ js/script.js
@@ -0,0 +1,21 @@
+/*var total = 0;
+var summands= [1,2,3,4,5];
+summands.forEach(function(summand){
+total +=summand;
+});
+alert(total); //15
+
+
+var friends= ['Georges', 'Jeni', 'Java'];
+friends.forEach(function(friend){
+alert('Hi ' + friend + ' !');
+});
+*/
+
+
+var multiply = 1;
+var timesArray = [1, 2, 3, 4, 5, 6, , 7];
+timesArray.forEach(function (timeArray) {
+  multiply *= timeArray;
+});
+alert(multiply);
